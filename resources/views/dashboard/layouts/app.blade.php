9bbc93fb9d5b056735d3045e98adaf359a2b4e67
Indra Ardika <indraardika@gmail.com>
2026-09-03 11:03:25 +0800

    LOGO

--- resources/views/dashboard/layouts/app.blade.php
+++ resources/views/dashboard/layouts/app.blade.php
@@ -12,7 +12,7 @@
     </title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="favicon" href="{{ asset('images/logo-samperin.png') }}">
+    <link rel="icon" href="{{ asset('assets/images/logo-samperin.png') }}">
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 
